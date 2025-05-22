b096bcd6ddb73613d5d6d235dac94199caeac08a
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2025-05-22 16:26:06 +0800

    changes

--- app/(hotel)/booking/[hotelId]/[room_category]/[room_type]/page.jsx
+++ app/(hotel)/booking/[hotelId]/[room_category]/[room_type]/page.jsx
@@ -1,31 +1,26 @@
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+const StepperBooking = dynamic(() => import('@/components/booking-page/stepper-booking'), {
+  ssr: false,
+});
+
 export const dynamic = 'force-dynamic';
 
-import CallToActions from "@/components/common/CallToActions";
-import Header11 from "@/components/header/header-11";
-import DefaultFooter from "@/components/footer/default";
-import StepperBooking from "@/components/booking-page/stepper-booking";
-
-export const metadata = {
-  title: "Hotel Booking Page || GoTrip - Travel & Tour React NextJS Template",
-  description: "GoTrip - Travel & Tour React NextJS Template",
-};
-
-const index = () => {
+export default function BookingPage() {
   return (
     <>
       <div className="header-margin"></div>
       <Header11 />
-
       <section className="pt-40 layout-pb-md">
         <div className="container">
-          <StepperBooking />
+          <Suspense fallback={<div>Loading...</div>}>
+            <StepperBooking />
+          </Suspense>
         </div>
       </section>
-
       <CallToActions />
       <DefaultFooter />
     </>
   );
-};
-
-export default index;
+}
