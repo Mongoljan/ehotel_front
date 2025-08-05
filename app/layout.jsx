96da19846e157babc4949e4fcf340c47b0462c3f
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2025-08-05 09:23:12 +0800

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Aos from "aos";
+import Script from "next/script";
 import { useEffect } from "react";
 import ScrollToTop from "../components/common/ScrollTop";
 import { TranslationProvider } from "../contexts/TranslationContext";
@@ -55,6 +56,24 @@ export default function RootLayout({ children }) {
             </Provider>
           </main>
         </TranslationProvider>
+        {/* Tawk.to Live Chat Widget */}
+        <Script
+          id="tawkto"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/68915c52a4fc79192a7ba7d0/1j1rt18nu';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
